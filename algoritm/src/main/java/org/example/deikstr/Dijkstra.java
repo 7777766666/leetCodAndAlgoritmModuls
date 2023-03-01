7e63b16256cda0131b112f0c722e1e5f225530a8
777 <goszakupki.aktiv@gmail.com>
2023-03-01 19:40:30 +0300

    Before change

--- algoritm/src/main/java/org/example/deikstr/Dijkstra.java
+++ algoritm/src/main/java/org/example/deikstr/Dijkstra.java
@@ -6,9 +6,10 @@ import java.util.LinkedHashMap;
 import java.util.Optional;
 import java.util.Set;
 
-public class Deikstr {
 
-    public void soutAll() {
+public class Dijkstra {
+
+    public void printAll() {
         System.out.println(finalTable.size()
                 + "  a element  " + finalTable.get("a")
                 + "  b element  " + finalTable.get("b")
@@ -16,32 +17,33 @@ public class Deikstr {
                 + "  d element  " + finalTable.get("d")
                 + "  arc size  " + arc.size()
         );
-
-
     }
 
     HashMap<String, Integer> arc = new LinkedHashMap<>();
 
     HashMap<String, Integer> finalTable = new LinkedHashMap<>();
 
-    public Deikstr() {
 
+    public Dijkstra() {
+
+        arc.put("AD", 5);
         arc.put("AB", 10);
-        arc.put("AD", 30);
-        arc.put("BC", 21);
-        arc.put("BD", 25);
-        arc.put("CD", 30);
+        arc.put("BD", 2);
+        arc.put("BC", 3);
+        arc.put("CD", 310);
+
 
         finalTable.put("a", 0);
         finalTable.put("b", null);    //10
         finalTable.put("c", null);    //30
         finalTable.put("d", null);    //31
+
     }
 
-    public int addBestElement() {
+
+    public void addBestElement() {
         int count = 0;
         int[] checkInt = new int[2];
-        String[] checkString = new String[2];
         String[] arcCheck = new String[2];
         int finalToTable = -1;
 
@@ -51,12 +53,10 @@ public class Deikstr {
                 break;
             }
             if (finalTable.get(allElements[i]) != null) {
-                checkString[count] = allElements[i];
                 String element = allElements[i];
                 Optional<String> pointChoose = point(element.toUpperCase());
                 if (pointChoose.isPresent()) {
                     arcCheck[count] = pointChoose.get().toUpperCase();
-
                     Integer arcWay = arc.get(pointChoose.get());
                     Integer intFinalTable = finalTable.get(element.toLowerCase());
                     int forChoose = arcWay + intFinalTable;
@@ -64,19 +64,15 @@ public class Deikstr {
 
                     System.out.println(Arrays.toString(checkInt));
                 }
-
             }
         }
         if (checkInt[1] == 0 || checkInt[0] <= checkInt[1]) {
             addAndDelete(arcCheck[0], allElements[finalToTable], checkInt[0]);
-            return checkInt[0];
         } else
             addAndDelete(arcCheck[1], allElements[finalToTable], checkInt[1]);
-        return checkInt[1];
-
     }
 
-    static String[] allElements = {"a", "b", "c", "d"};
+    private static final String[] allElements = {"a", "b", "c", "d"};
 
     public String pointFistName() {
         String element = "";
@@ -87,17 +83,6 @@ public class Deikstr {
             }
         }
         return element;
-    }
-
-    public String pointSecondName(String str1) {
-        String str2 = "";
-        for (int i = 0; i < allElements.length; i++) {
-            if (allElements[i].equalsIgnoreCase(str1)) {
-                str2 = allElements[i + 1].toUpperCase();
-                break;
-            }
-        }
-        return str2;
     }
 
     public String pointFinal() {
@@ -135,22 +120,22 @@ public class Deikstr {
         arc.remove(toNextPoint);
     }
 
-
     public static void main(String[] args) {
-        Deikstr deikstr = new Deikstr();
+        Dijkstra dijkstra = new Dijkstra();
 
-        String strFromArr = deikstr.pointFistName();
+        String strFromArr = dijkstra.pointFistName();
         System.out.println(strFromArr + "   it is strFromArr");
-        String toNextPoint = deikstr.findPoint(strFromArr);
+        String toNextPoint = dijkstra.findPoint(strFromArr);
         System.out.println(toNextPoint + "  number after find point toNextPoint");
-        System.out.println(deikstr.pointFinal() + "   point for merge FINAL");
-        Integer numForPoint = deikstr.arc.get(toNextPoint);
-        deikstr.addAndDelete(toNextPoint, deikstr.pointFinal(), numForPoint);
+        System.out.println(dijkstra.pointFinal() + "   point for merge FINAL");
+        Integer numForPoint = dijkstra.arc.get(toNextPoint);
+        dijkstra.addAndDelete(toNextPoint, dijkstra.pointFinal(), numForPoint);
 
-        deikstr.addBestElement();
-        deikstr.soutAll();
-        deikstr.addBestElement();
-        deikstr.soutAll();
+        dijkstra.addBestElement();
+        dijkstra.printAll();
+        dijkstra.addBestElement();
+        dijkstra.printAll();
+
 
 
     }
