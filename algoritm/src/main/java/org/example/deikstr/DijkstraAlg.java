d14445eac6eac15045a2cedb94982fac2fde8e0e
777 <goszakupki.aktiv@gmail.com>
2023-03-02 02:46:02 +0300

    return [AB, AD, null, null, null]  arcPointReturn

--- algoritm/src/main/java/org/example/deikstr/DijkstraAlg.java
+++ algoritm/src/main/java/org/example/deikstr/DijkstraAlg.java
@@ -10,6 +10,12 @@ public class DijkstraAlg {
     private static final String[] arcString = {"AB", "AD", "BC", "CD", "BD"};
     HashMap<Integer, String> hashInt = new HashMap<>();
     HashMap<String, Integer> hashStr = new HashMap<>();
+    HashMap<String, Integer> hashFinal = new HashMap<>();
+
+    public DijkstraAlg()
+    {
+        hashFinal.put(arcString[0], 0);
+    }
 
     public static void addToHash(int[] arcInt, String[] arcString,
                                  HashMap<Integer, String> hashInt, HashMap<String, Integer> hashStr) {
@@ -30,8 +36,22 @@ public class DijkstraAlg {
         String [] pointAll = uniqueCharacters.split(" ");
         System.out.println(Arrays.toString(pointAll) + " AllPoints arr");
         return pointAll;
+    }
 
+    public static String[] arcPoint(String[] arcString, HashMap<String, Integer> hashFinal, String[] pointAll){
+        int w = 0;
+        String[] arcs = new String[arcString.length];
+        if (pointAll.length == hashFinal.size()) {
+            System.out.println("BINGO, I hope!");
+        }else {
+            for (String xxx : arcString) {
+                if (xxx.contains(pointAll[hashFinal.size() - 1])) {
+                    arcs[w++] = xxx;
+                }
 
+            }
+        }
+        return arcs;
     }
 
 
@@ -41,6 +61,11 @@ public class DijkstraAlg {
         addToHash(arcInt, arcString, alg.hashInt, alg.hashStr); //put elements to 2 hashMap mirror
 
         String[] pointAll = pointArr(arcString); // all points (Up letters)
+
+        String[] arcPointReturn = arcPoint(arcString, alg.hashFinal, pointAll);
+        System.out.println(Arrays.toString(arcPointReturn) + "  arcPointReturn");
+
+        System.out.println(Arrays.toString(arcString) + "  arcString");
 
 
     }
