4c3ac1e2d97dc586a6dc49ee69cd14e4dbd012d2
777 <goszakupki.aktiv@gmail.com>
2023-03-04 06:38:24 +0300

    сделал возврат значения минимально элемента

--- algoritm/src/main/java/org/example/deikstr/DijkstraAlg.java
+++ algoritm/src/main/java/org/example/deikstr/DijkstraAlg.java
@@ -3,11 +3,11 @@ package org.example.deikstr;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class DijkstraAlg {
+//    private final static String startPoint = "A";
 
     private static final int[] arcInt = {10, 5, 3, 100, 2, 0};
     private static final String[] arcString = {"AB", "AD", "BC", "CD", "BD", "AA"};
@@ -27,46 +27,26 @@ public class DijkstraAlg {
     }
 
     public static String[] pointArr(String[] arcString) {
-        String uniqueCharacters = Arrays.stream(arcString)
+
+        String[] pointAll = Arrays.stream(arcString)
                 .flatMapToInt(CharSequence::chars)
                 .distinct()
-                .mapToObj(x -> Character.toString((char) x))
-                .collect(Collectors.joining(" "));
-        String[] pointAll = uniqueCharacters.split(" ");
+                .sorted()
+                .mapToObj(x -> String.valueOf((char) x))
+                .toArray(String[]::new);
+
         System.out.println(Arrays.toString(pointAll) + " AllPoints arr");
         return pointAll;
     }
-    //return [AB, AD, null, null, null, AA] for A
-//    public static String[] arcPoint(String[] arcString, HashMap<String, Integer> hashFinal, String[] pointAll) {
-//        String[] arcs = new String[arcString.length];
-//        if (pointAll.length == hashFinal.size()) {
-//            System.out.println("BINGO, I hope!");
-//        } else {
-//            for (int i = 0; i <= hashFinal.size(); i++) {
-//                String element = pointAll[i];
-//                System.out.println(arcs[i] + " ##############");
-//                for (int j = 0; j < arcString.length; j++) {
-//                    if (arcString[j].contains(element)) {
-//                        //стрим фильтрующий по элементам
-//
-//
-//                        arcs[j] = arcString[j];
-//                    }
-//                }
-//            }
-//        }
-//        System.out.println(Arrays.toString(arcs) + " arcs A letter");
-//        return arcs;
-//    }
 
 
-    public static String[] shortest(HashMap<String, Integer> hashFinal, String[] pointAll, String[] pointFinalStr) {
+    public static String[] arcWithPoint(HashMap<String, Integer> hashFinal, String[] pointAll, String[] pointFinalStr) {
         String[] forChooseArc = new String[arcString.length];
         if (hashFinal.size() == 0) {
-            hashFinal.put(pointAll[0], 0);
             pointFinalStr[0] = pointAll[0];
-        }
+            hashFinal.put(pointFinalStr[0], 0);
 
+        }
         for (int i = 0; i < pointFinalStr.length; i++) {
             if (pointFinalStr[i] != null) {
                 for (int w = 0; w < arcString.length; w++) {
@@ -74,11 +54,39 @@ public class DijkstraAlg {
                         forChooseArc[w] = arcString[w]; //all arc, which contains pointFinalStr
                     }
                 }
-
             }
-
-        }  return forChooseArc;
+        }
+        return forChooseArc;
     }
+
+    public static Integer minValue(String[] shortestLength, String[] pointFinalStr,
+                                   HashMap<String, Integer> hashFinal, HashMap<String, Integer> hashStr) {
+
+        Integer[] minWay = new Integer[shortestLength.length];
+        for (int i = 0; i < pointFinalStr.length; i++) {
+            if (pointFinalStr[i] != null) {
+                for (int w = 0; w < shortestLength.length; w++) {
+                    if (shortestLength[w] != null) {
+                        Integer intPoint = hashFinal.get(pointFinalStr[i]);
+                        Integer intArc = hashStr.get(shortestLength[w]);
+                        Integer pointPlusArc = (intPoint + intArc);
+                        minWay[w] = pointPlusArc;
+                    }
+
+
+                }
+            }
+        }
+        return Arrays.stream(minWay)
+                .filter(Objects::nonNull)
+//                .distinct()
+                .min(Integer::compareTo)
+                .orElse(-1000);
+
+
+    }
+
+
 
     public static String[] arcToLetters(String[] arcPointReturn) {  //принимаем ребра, возвращаем точки
         ArrayList<String> toList = Arrays.stream(arcPointReturn)
@@ -94,20 +102,22 @@ public class DijkstraAlg {
     public static void main(String[] args) {
         DijkstraAlg alg = new DijkstraAlg();
 
-
         addToHash(arcInt, arcString, alg.hashInt, alg.hashStr); //put elements to 2 hashMap mirror
 
-        final String[] pointAll = pointArr(arcString); // all points (Up letters)
+        final String[] pointAll = pointArr(arcString); // all points sorted (Up letters)
         final String[] pointFinalStr = new String[pointAll.length]; //array of real Points with null initial
+
 
 //        String[] arcPointReturn = arcPoint(arcString, alg.hashFinal, pointAll);  //[AB, AD, null, null, null, AA] for A
 
-        String[] shortestLength = shortest(alg.hashFinal, pointAll, pointFinalStr);
+        String[] shortestLength = arcWithPoint(alg.hashFinal, pointAll, pointFinalStr);
 
-
+        System.out.println((Arrays.toString(pointFinalStr)) + "    pointFinalStr");
+        System.out.println((alg.hashFinal.get(pointFinalStr[0])) + "   pointFinalStr get from hashFinal");
         System.out.println(Arrays.toString(shortestLength) + "  shortestLength");
+//        System.out.println((Arrays.toString(sumPoint(shortestLength, pointAll, alg.hashFinal, alg.hashStr))) + "  result int");
 
-
+        System.out.println(((minValue(shortestLength, pointFinalStr, alg.hashFinal, alg.hashStr))) + " == A");
 
 
     }
