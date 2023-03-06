c9d9a916f1cd983b3b051a15da311f4c90728e9e
777 <goszakupki.aktiv@gmail.com>
2023-03-06 06:54:16 +0300

    задача про возврат части строки

--- codewars/src/main/java/org/example/App.java
+++ codewars/src/main/java/org/example/App.java
@@ -1,18 +1,24 @@
 package org.example;
 
+import org.example.day1.FindStringInString;
 import org.example.day1.ReturnPhone;
 import org.example.day1.ReturnPhoneNumber;
 
-public class App
-{
-    public static void main( String[] args )    {
+public class App{
+        public static void main( String[] args ) {
+
+            String big = "leetcode";
+            String small = "leeto";
+            FindStringInString findStringInString = new FindStringInString();
+            System.out.println(findStringInString.strStr(big, small));
+
+
 
         int[] arr10 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
 //        System.out.println(ReturnPhoneNumber.createPhoneNumber(arr10));
 //        System.out.println(ReturnPhone.createPhoneNumber(arr10));
 
-        String big = "smallbutsmall";
-        String small = "small";
+
 
 
     }
--- codewars/src/main/java/org/example/day1/FindStringInString.java
+++ codewars/src/main/java/org/example/day1/FindStringInString.java
@@ -1,13 +1,72 @@
 package org.example.day1;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class FindStringInString {
 
-        public int strStr(String haystack, String needle) {
+    public int strStr(String haystack, String needle) {
 
+        String[] a = haystack.split("");
+        String[] b = needle.split("");
 
-            return 777;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b.length; j++) {
+                if (a[i].equals(b[j])) {
+                    for (int k = 1; k < needle.length(); k++) {
+
+                            a[i + k].equals(b[j + 1]);
+
+                    }
+                    return i;
+                }
+            }
+
         }
-
-
+        return -1;
+    }
 
 }
+
+
+//        String[] a = {haystack};
+//        String[] b = {needle};
+//
+//        String[] c = haystack.split(needle);
+//        System.out.println((Arrays.toString(c)) + " c");
+//        System.out.println(haystack.length() );
+//        System.out.println((c.length) + "  c.length");
+//        if (haystack.length() == c.length) {
+//            return -1;
+//        } else {
+//            return c[0].length();
+//        }
+
+
+//        String[] d = haystack.split("");
+//
+//        int length = c[0].length();
+//        System.out.println(length + "   !!!!!!!!!!!!!!");
+//
+//
+//        String[] b = {needle};
+//        String[] f = needle.split("");
+//        System.out.println((needle.length()));
+//        System.out.println((haystack.length()));
+//        String[] a = {haystack};
+//        System.out.println((Arrays.toString(a)) + " a");
+//        System.out.println((Arrays.toString(c)) + "  c");
+//        System.out.println(((haystack.length())) + "  haystack");
+//
+//        if (a[0].contains(b[0])) {
+//
+//            for (int i = 0; i < c.length; i++){
+////                if (c[i] == null) {
+////                    return i;
+//
+//            }
+//        }
+//
+//
+//        return -1;
+
